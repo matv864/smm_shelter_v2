a2e7201d738eb64632cac91661c915ff1c27c414
matv864 <matv864@gmail.com>
2025-07-02 00:15:40 +1000

    add manager

--- backend/src/adapters/database/models/__init__.py
+++ backend/src/adapters/database/models/__init__.py
@@ -1,6 +1,7 @@
 from .article import Article, ArticleContent
 from .base import Base
 from .enums import GenderEnum
+from .manager import Manager
 from .news import News, NewsContent
 from .pet import Pet, PetContent
 from .pet_type import PetType
@@ -12,6 +13,7 @@ __all__ = [
     "Base",
     "GenderEnum",
     "Article", "ArticleContent",
+    "Manager",
     "News", "NewsContent",
     "Pet", "PetContent",
     "PetType",
