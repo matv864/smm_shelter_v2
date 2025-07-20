7040e83a0adbcb06681737520f96f2a6d7b371ff
matv864 <matv864@gmail.com>
2025-07-20 17:13:02 +1000

    delete useless exceptions

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,5 @@
 from fastadmin import fastapi_app as admin_app
 from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
@@ -8,14 +7,7 @@ from src.api import main_api_router
 
 from src.settings import settings
 from src.utils.exceptions import (
-    AccessDenied,
-    AuthorizationException,
-    PlaceOrderForeignKeyError,
     ResultNotFound,
-    UserAlreadyExist,
-    UserNotRegistered,
-    VerificationException,
-    WrongCredentials,
 )
 
 app = FastAPI(
@@ -41,64 +33,3 @@ async def not_found_exception_handler(request: Request, exc: ResultNotFound):
         content={"detail": "Result not found"},
     )
 
-
-@app.exception_handler(AuthorizationException)
-async def authorization_exception_handler(
-    request: Request, exc: AuthorizationException
-):
-    return JSONResponse(
-        status_code=401,
-        content={"detail": "Provided token mailformed"},
-    )
-
-
-@app.exception_handler(WrongCredentials)
-async def login_exception_handler(request: Request, exc: WrongCredentials):
-    return JSONResponse(
-        status_code=401,
-        content={"detail": "Wrong credentials provided"},
-    )
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=400,
-        content={"detail": exc.errors(), "body": exc.body},
-    )
-
-
-@app.exception_handler(VerificationException)
-async def verification_exception_handler(request: Request, exc: VerificationException):
-    return JSONResponse(
-        status_code=401,
-        content={"detail": "Verification code is incorrect"},
-    )
-
-
-@app.exception_handler(UserNotRegistered)
-async def user_not_registered(request: Request, exc: UserNotRegistered):
-    return JSONResponse(
-        status_code=403, content={"detail": "No authorization tokens provided"}
-    )
-
-
-@app.exception_handler(PlaceOrderForeignKeyError)
-async def place_orders_foreign_key_error(
-    request: Request, exc: PlaceOrderForeignKeyError
-):
-    return JSONResponse(status_code=400, content={"detail": "Place order FK error"})
-
-
-@app.exception_handler(AccessDenied)
-async def access_denied(request: Request, exc: AccessDenied):
-    return JSONResponse(
-        status_code=403, content={"detail": "You have no right to access this resource"}
-    )
-
-
-@app.exception_handler(UserAlreadyExist)
-async def user_already_exist(request: Request, exc: AccessDenied):
-    return JSONResponse(
-        status_code=409, content={"detail": "User with this credential already exist"}
-    )
